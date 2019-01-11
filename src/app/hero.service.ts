692ac79c8b0a9638ba11d25c314d2dae3cfb36bc
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2019-01-10 21:53:21 -0500

    Add in-memory data service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
- 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
  
 import { Hero } from './hero';
@@ -9,7 +9,9 @@ import { MessageService } from './message.service';
 @Injectable({ providedIn: 'root' })
 export class HeroService {
  
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
  
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
